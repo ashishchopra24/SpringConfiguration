3e3fc738db6d69db661cac09a6e025a3d98a03b6
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-08-16 00:57:28 +0530

    XML Config

--- src/main/java/com/example/springconfiguration/SpringConfigurationApplication.java
+++ src/main/java/com/example/springconfiguration/SpringConfigurationApplication.java
@@ -1,13 +1,22 @@
 package com.example.springconfiguration;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:Configuration.xml")
 public class SpringConfigurationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringConfigurationApplication.class, args);
+       ApplicationContext ctx= SpringApplication.run(SpringConfigurationApplication.class, args);
+        Country object=(Country)ctx.getBean("country");
+        System.out.println();System.out.println();System.out.println();System.out.println();System.out.println();
+
+        System.out.println("Country Name "+object.getName());
+        System.out.println("Population "+object.getPopulation());
     }
 
 }
